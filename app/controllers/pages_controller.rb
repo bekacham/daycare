0764c7eaab26474bae40c867e064ea2daaeefccf
Rebekah Chambers <bekachambers@gmail.com>
2017-10-14 11:57:09 -0600

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def babies
+  end
+
+  def kids
+  end
+
+  def pricing
+  end
+
+  def menu
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/babies'
+
+  get 'pages/kids'
+
+  get 'pages/pricing'
+
+  get 'pages/menu'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
