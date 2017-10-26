084d13e088ff2dbbd78ee2f97254c8556c63b7e6
Rebekah Chambers <bekachambers@gmail.com>
2017-10-25 20:26:18 -0600

    added nav bar and default content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,17 +3,22 @@ class PagesController < ApplicationController
   end
 
   def about
+    @page_title = "About me"
   end
 
   def babies
+    @page_title = "Baby Program"
   end
 
   def kids
+    @page_title = "Toddler Program"
   end
 
   def pricing
+    @page_title = "Childcare Prices"
   end
 
   def menu
+    @page_title = "Weekly Menu"
   end
 end
